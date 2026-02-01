b08f29a3c17eabf9679c3814979bb84ed5710e92
WolvenStark <wolvenstark@gmail.com>
2026-02-01 11:05:28 +1100

    Update controls to mention E, Q and SPACE

--- Wooft/Assets/Scripts/DialogueController.cs
+++ Wooft/Assets/Scripts/DialogueController.cs
@@ -1,10 +1,7 @@
-using FMOD;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
